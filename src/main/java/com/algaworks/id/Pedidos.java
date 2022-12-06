8621b244070cf50693672ce8eeb7e99406726b6f
Arnaldo Neto <vilela-neto@hotmail.com>
2022-12-05 22:32:40 -0300

    Como funciona a Injeção de Dependências - pt 3

--- src/main/java/com/algaworks/id/Pedidos.java
+++ src/main/java/com/algaworks/id/Pedidos.java
@@ -2,8 +2,14 @@ package com.algaworks.id;
 
 import java.math.BigDecimal;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
 public class Pedidos {
 
+	@Inject
+	private EntityManager manager;
+	
 	public BigDecimal totalPedidosMesAtual() {
 		return new BigDecimal("100");
 	}
