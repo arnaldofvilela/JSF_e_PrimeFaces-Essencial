9fe672bd67099634c3c96378a187ffdc2c84a1f2
Arnaldo Neto <vilela-neto@hotmail.com>
2022-12-16 23:35:09 -0300

    Conversão e validação

--- src/main/java/com/algaworks/erp/model/Empresa.java
+++ src/main/java/com/algaworks/erp/model/Empresa.java
@@ -1,6 +1,7 @@
 package com.algaworks.erp.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -37,7 +38,7 @@ public class Empresa implements Serializable {
 	@Column(nullable = false, length = 18)
 	private String cnpj;
 	
-	//@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.DATE)
 	@Column(name = "data_fundacao")
 	private Date dataFundacao;
 	
@@ -49,6 +50,17 @@ public class Empresa implements Serializable {
 	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
 	
+	@Column(precision = 10, scale = 2)
+	private BigDecimal faturamento;
+	
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
+	}
+
 	public Long getId() {
 		return id;
 	}
